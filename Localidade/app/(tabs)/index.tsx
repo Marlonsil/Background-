9b0338314cfc05af2cdb8d812ec7ae7464183e71
Marlonsil <marlonsilveira397@gmail.com>
2024-08-21 17:45:08 +0000

    save2

--- Localidade/app/(tabs)/index.tsx
+++ Localidade/app/(tabs)/index.tsx
@@ -1,26 +1,16 @@
 import { View, Image, StyleSheet, Platform } from 'react-native';
-
+import Home from '../telas/home'
 
 export default function HomeScreen() {
   return (
-    <View></View>
+    <View style={styles.container}>
+      <Home/>
+    </View>
   );
 }
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+ 
 });
